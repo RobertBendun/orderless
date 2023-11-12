3065630c3169506782b79f48085086b1b9e06243
Robert Bendun <robert@bendun.cc>
2023-11-12 01:44:23 +0100

    stress testing

--- orderless_example.rs
+++ orderless_example.rs
@@ -23,3 +23,8 @@ fn all_permutations() {
     assert!(match (30, 10, 20) { orderless!((10, 20, 30)) => true, _ => false });
     assert!(match (30, 20, 10) { orderless!((10, 20, 30)) => true, _ => false });
 }
+
+#[test]
+fn stress_testing() {
+    assert!(match (1, 2, 3, 4, 5, 6, 7) { orderless!((7, 6, 5, 4, 3, 2, 1)) => true, _ => false });
+}
